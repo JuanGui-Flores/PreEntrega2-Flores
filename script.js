d9cf7672e545683afd0c601e5d744fcc9d65a0f5
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-06-06 10:40:39 -0300

    a

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 function calcular() {
-    var edad = parseInt(document.getElementById('edad').value);
-    var genero = document.getElementById('genero').value;
-    var cobertura = document.getElementById('cobertura').value;
+    var edad = parseInt(prompt("Ingrese su edad:"));
+    var genero = prompt("Ingrese su género (masculino/femenino):");
+    var cobertura = prompt("Ingrese el tipo de cobertura (basica/premium):");
 
-    var factorEdad = 0;
-    var factorGenero = 0;
-    var factorCobertura = 0;
-
-    if (edad >= 18 && edad <= 30) {
-        factorEdad = 0.5;
-    } else if (edad > 30 && edad <= 50) {
-        factorEdad = 1;
-    } else if (edad > 50) {
-        factorEdad = 1.5;
-    }
-
-    if (genero === 'masculino') {
-        factorGenero = 1.2;
-    } else if (genero === 'femenino') {
-        factorGenero = 0.8;
-    }
-
-    if (cobertura === 'basica') {
-        factorCobertura = 1;
-    } else if (cobertura === 'premium') {
-        factorCobertura = 1.5;
-    }
+    var factorEdad = obtenerFactorEdad(edad);
+    var factorGenero = obtenerFactorGenero(genero);
+    var factorCobertura = obtenerFactorCobertura(cobertura);
 
     var precioBase = 100;
-    var precioFinal = precioBase * factorEdad * factorGenero * factorCobertura;
+    var precioFinal = calcularPrecioFinal(precioBase, factorEdad, factorGenero, factorCobertura);
 
-    document.getElementById('resultado').innerHTML = "El precio del seguro es: $" + precioFinal.toFixed(2);
+    mostrarResultado(precioFinal);
+}
+
+function obtenerFactorEdad(edad) {
+    if (edad >= 18 && edad <= 30) {
+        return 0.5;
+    } else if (edad > 30 && edad <= 50) {
+        return 1;
+    } else if (edad > 50) {
+        return 1.5;
+    } else {
+        return 0;
+    }
+}
+
+function obtenerFactorGenero(genero) {
+    if (genero === 'masculino') {
+        return 1.2;
+    } else if (genero === 'femenino') {
+        return 0.8;
+    } else {
+        return 0;
+    }
+}
+
+function obtenerFactorCobertura(cobertura) {
+    if (cobertura === 'basica') {
+        return 1;
+    } else if (cobertura === 'premium') {
+        return 1.5;
+    } else {
+        return 0;
+    }
+}
+
+function calcularPrecioFinal(precioBase, factorEdad, factorGenero, factorCobertura) {
+    return precioBase * factorEdad * factorGenero * factorCobertura;
+}
+
+function mostrarResultado(precioFinal) {
+    alert("El precio del seguro es: $" + precioFinal.toFixed(2));
 }
